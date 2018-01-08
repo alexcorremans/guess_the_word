8aeddcc1280cff9c9cd3559d9e9c72867cd787a4
Alexandra Corremans <alexandra.corremans@gmail.com>
2018-01-08 21:30:10 +0100

    done!

--- guess_the_word.rb
+++ guess_the_word.rb
@@ -7,22 +7,26 @@ all_words.each do |word|
 end
 
 puts "Are you ready to guess the word?"
-puts "Type 'new' to play a new game or 'saved' to open a previously saved game"
-choice = gets.chomp.downcase
-until choice == "new" || choice == "saved"
-  puts "Not a valid choice, choose 'new' or 'saved':"
-  choice = gets.chomp.downcase
-end
-if choice == "new"
+if Dir["saved_games/*"].empty?
   Game.new(words).start
 else
-  puts "What name did you save your game as?"
-  begin
-    filename = gets.chomp.downcase
-    game = YAML.load_file("saved_games/#{filename}.yml")
-    game.play
-  rescue
-    puts "No saved files with that name. Try again:"
-    retry
+  puts "Type 'new' to play a new game or 'saved' to open a previously saved game"
+  choice = gets.chomp.downcase
+  until choice == "new" || choice == "saved"
+    puts "Not a valid choice, choose 'new' or 'saved':"
+    choice = gets.chomp.downcase
+  end
+  if choice == "new"
+    Game.new(words).start
+  else
+    puts "What name did you save your game as?"
+    begin
+      filename = gets.chomp.downcase
+      game = YAML.load_file("saved_games/#{filename}.yml")
+      game.play
+    rescue
+      puts "No saved files with that name. Try again:"
+      retry
+    end
   end
 end
